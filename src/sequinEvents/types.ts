c42303d5dc2cea5665e94b2ac6c65345f8cc11b0
Kayky de Brito <oss@kaykyb.com>
2022-04-04 22:41:46 -0300

    Add SequinEvent typings

--- src/sequinEvents/types.ts
+++ src/sequinEvents/types.ts
@@ -1,1 +1,35 @@
 export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+
+interface BaseEvent {
+  id: string;
+  inserted_at: string;
+  object_type: string;
+}
+
+interface InsertEvent<T> extends BaseEvent {
+  event_type: "insert";
+  payload: {
+    record_id: string;
+    record: T;
+  };
+}
+
+interface UpdateEvent<T> extends BaseEvent {
+  event_type: "update";
+  payload: {
+    record_id: string;
+    record: T;
+    old_record: T;
+    changes: Partial<T>;
+  };
+}
+
+interface DeleteEvent<T> extends BaseEvent {
+  event_type: "delete";
+  payload: {
+    record_id: string;
+    record: T;
+  };
+}
+
+export type SequinEvent<T> = InsertEvent<T> | UpdateEvent<T> | DeleteEvent<T>;
